e1544f509587ea057016becc3e30ea51c9e344f5
Bhanupratapsingh21 <bhanupss137@gmail.com>
2024-05-02 20:16:02 +0530

    new

---
+++ Basic's/src/file.js
@@ -0,0 +1,25 @@
+// means do oprations on files 
+// fs 
+const fs = require("fs")
+
+// sync
+//fs.writeFileSync("./test.txt","hey thare");
+
+// async
+/* fs.writeFile("./test.txt","hey thare",(err)=>{
+    //console.log(err)
+})
+
+const filetext =  fs.readFileSync("./contact.txt","utf-8");
+console.log(filetext);
+fs.readFile("./contact.txt","utf-8",(err,res)=>{
+    if(err){
+        console.log("error")
+    }else{
+        console.log(res)
+    }
+});
+
+// diff in both is astnc or sync is we got a call back async or we can handle that by try catch
+*/
+fs.appendFileSync("./test.txt", "hey there\n", { encoding: 'utf8', flag: 'a' });
--- Basic's/src/index.js
+++ Basic's/src/index.js
@@ -1,1 +1,8 @@
+const {add , less} = require("./math")
+
+
 console.log("Hello Sir")
+
+console.log(add(1,3))
+
+console.log(less(1,3))
