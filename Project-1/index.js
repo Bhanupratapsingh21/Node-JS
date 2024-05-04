c9c539da13b2f0561e1e53b27f834cf347a7aef6
Bhanupratapsingh21 <bhanupss137@gmail.com>
2024-05-04 21:41:46 +0530

    assiment completed

--- Project-1/index.js
+++ Project-1/index.js
@@ -33,24 +33,54 @@ app.get("/users",(req,res)=>{
 
 app.route("/api/user/:id")
 .get((req,res)=>{
-    
     const id = Number(req.params.id);
     const user = users.find((user) => user.id === id);
     return res.json(user);
-
 })
 .patch((req,res)=>{
-    return res.json({status : "panding"})
+    const id = Number(req.params.id);
+    const newuser = req.body
+    let updateduser =  {...users[id-1],...newuser}
+    users[id-1] = updateduser
+
+    fs.writeFile("./MOCK_DATA.json" , JSON.stringify(users), (err,res)=>{
+        if(err){
+            res.json({ massage : "Something Want wrong"})
+        }
+    })
+    return res.json({status : "Successfully updateduser",...users[id-1]})
 })
 .post((req,res)=>{
-    return res.json({status : "panding"})
+    const id = Number(req.params.id);
+    const newuser = req.body
+    let updateduser =  {...users[id-1],...newuser}
+    users[id-1] = updateduser
+
+    fs.writeFile("./MOCK_DATA.json" , JSON.stringify(users), (err,res)=>{
+        if(err){
+            res.json({ massage : "Something Want wrong"})
+        }
+    })
+    return res.json({status : "Successfully updateduser",...users[id-1]})
 })
 .delete((req,res)=>{
-    return res.json({status : "panding"})
+    const id = Number(req.params.id);
+    const indexToRemove = users.findIndex(user => user.id === id); // Assuming users is an array of objects with an 'id' property
+    if (indexToRemove !== -1) {
+        users.splice(indexToRemove, 1); // Remove the element at indexToRemove
+        fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err) => {
+            if (err) {
+                res.status(500).json({ message: "Something went wrong" });
+            } else {
+                res.json({ status: "Successfully removed", id: id });
+            }
+        });
+    } else {
+        res.status(404).json({ message: "User not found" });
+    }
 })
 
 app.post("/api/users",(req,res)=>{
-
     // console.log(req.body);
     const body = req.body;
     users.push({...body, id : users.length+1})
@@ -62,14 +92,5 @@ app.post("/api/users",(req,res)=>{
     return res.json({status : "Successfully added",id: users.length})
 
 });
-
-app.route("/api/users/:id",(req,res)=>{
-    return res.json({status : "panding"})
-});
-
-app.patch("/api/users/:id",(req,res)=>{
-    return res.json({status : "panding"})
-});
-
 
 // create this full app by own
