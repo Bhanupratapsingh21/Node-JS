8f0ee99b950747de86ddaca43a299c18eadbb98f
Bhanupratapsingh21 <bhanupss137@gmail.com>
2024-05-15 21:26:00 +0530

    Create Signin with jwt or bcrypt

--- Auth/Controllers/User-controllers.js
+++ Auth/Controllers/User-controllers.js
@@ -1,12 +1,49 @@
-async function handleUserLogin (req,res){
-    res.json({msg:"login"})
-}
+import { User } from "../Models/User.Models.js"
+import jwt from "jsonwebtoken"
 
-async function handleUserSignUp (req,res){
-    res.json({msg:"Sign-Up"})
+async function handleUserLogin(req, res) {
+    res.json({ msg: "Sign-Up" })
+
+}
+// added a save method in models so when i save password it got increpts
+async function handleUserSignUp(req, res) {
+
+    const { name, email, password } = req.body
+    if (
+        [name, email, password].some((field) => field.trim() === "")
+    ) {
+        return res.status(400).json({ "msg": "All Fields Are Required" })
+    }
+    // await here
+    const exitedUser = await User.findOne({
+        $or: [{ name }, { email }]
+    })
+
+    if (exitedUser) {
+        return res.status(409).json({ "msg": "Name Or Email Already Exists" })
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+    })
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+    if (!createdUser) {
+        return res.status(500).json({ "msg": "Something Want Wrong While Registering User" })
+    }
+
+    return res.status(201).json({
+        msg: "User Registered SuccesFully",
+        createdUser,
+    })
 }
 
 export {
     handleUserSignUp,
     handleUserLogin
 }
+
--- Auth/Models/User.Models.js
+++ Auth/Models/User.Models.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 const userSchema = new mongoose.Schema(
     {
@@ -19,6 +21,44 @@ const userSchema = new mongoose.Schema(
     { timestamps : true}
 )
 
-const User = mongoose.model("user" , userSchema)
+// some for authentications etc 
 
-export default User
+userSchema.pre("save", async function (next){
+    if(!this.isModified("password")) return next()
+
+    this.password = await bcrypt.hash(this.password,10)
+    next();
+
+})
+
+userSchema.methods.isPasswordCorrect = async function (password){
+    return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.genrateAccessToken = function (){
+    return jwt.sign(
+        {
+            _id:this._id,
+            email: this.email,
+            name: this.name,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.genrateRefreshToken = function (){
+    return jwt.sign(
+        {
+            _id:this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model("user" , userSchema)
